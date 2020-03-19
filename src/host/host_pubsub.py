b090a6c12a608d3fbf56b296f71a8a2f4c1e6635
paul <pyeh.tw@icloud.com>
2020-03-19 21:18:03 +0100

    pubsub node for both devices structure complete

--- src/host/host_pubsub.py
+++ src/host/host_pubsub.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
-# def talker():
-#     pub = rospy.Publisher('/host_msg', String, queue_size=10)
-#     rate = rospy.Rate(1) # 10hz
-#     while not rospy.is_shutdown():
-#         hello_str = "From host %s" % rospy.get_time()
-#         # rospy.loginfo(hello_str)
-#         pub.publish(hello_str)
-#         listener()
-#         rate.sleep()
+from sensor_msgs.msg import Image
 
 def callback(data):
+    # rospy.loginfo("callback")
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def listener():
+def talker():
     rospy.init_node('host_pub_sub', anonymous=True)
-    rospy.Subscriber("/pi_msg", String, callback)
-    # talker()
-    rospy.shostn()
+    pub = rospy.Publisher('/host_msg', String, queue_size=10)
+    rate = rospy.Rate(1) # 10hz
+
+    while not rospy.is_shutdown():
+        hello_str = "From host %s" % rospy.get_time()
+        rospy.loginfo("host msg sent")
+        pub.publish(hello_str)
+        rospy.Subscriber('/pi_msg', String, callback)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
-        listener()
+        talker()
     except rospy.ROSInterruptException:
         pass
--- src/pi/pi_pubsub.py
+++ src/pi/pi_pubsub.py
@@ -9,6 +9,10 @@ from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import time
 
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+
 def capture_image(camera):
     image = np.empty((240*320*3,),dtype=np.uint8)
     camera.capture(image, 'bgr')
@@ -18,7 +22,7 @@ def capture_image(camera):
 
 def talker():
     rospy.init_node('pi_pub_sub', anonymous=True)
-    pub = rospy.Publisher('/pi_msg', String, queue_size=10)
+    pub = rospy.Publisher('/pi_msg', String, queue_size=100)
     pub_image = rospy.Publisher('/cam_msg', Image, queue_size=10)
     rate = rospy.Rate(1) # 10hz
     bridge = CvBridge()
@@ -28,21 +32,12 @@ def talker():
 
     while not rospy.is_shutdown():
         hello_str = "From pi %s" % rospy.get_time()
-        rospy.loginfo("pi msg sent")
         image_ros = bridge.cv2_to_imgmsg(capture_image(camera), encoding="bgr8")
         pub_image.publish(image_ros)
         pub.publish(hello_str)
-        # listener()
+        rospy.loginfo("pi msg sent")
+        rospy.Subscriber('/host_msg', String, callback)
         rate.sleep()
-
-# def callback(data):
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
-# def listener():
-#     rospy.init_node('pi_pub_sub', anonymous=True)
-#     rospy.Subscriber("/pi_msg", String, callback)
-#     talker()
-#     rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
